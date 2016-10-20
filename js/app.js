77caefb49745e04ccf55124f2fd4e405cfafc241
Ruben AvilaH <rubenavilah@gmail.com>
2016-10-19 21:42:47 -0500

    offcanvas right ready

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
   console.log("ready");
+
+  // The Toggle Off Canvas
+
+  $(function() {
+      // Toggle Nav on Click
+      $('.toggle-nav').click(function() {
+          // Calling a function in case you want to expand upon this.
+          toggleNav();
+          console.log("click!");
+      });
+  });
+  function toggleNav() {
+      if ($('#wrapper').hasClass('show-nav')) {
+          // Do things on Nav Close
+          $('#wrapper').removeClass('show-nav');
+      } else {
+          // Do things on Nav Open
+          $('#wrapper').addClass('show-nav');
+      }
+  }
+
+
+
+
 });
