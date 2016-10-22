d2b76e253a527fead711e7421ac0eead37b8ea5e
Rodrigo <rodrigotos3@hotmail.com>
2016-10-21 21:41:59 -0500

    mini map colombia

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-// Crea el mapa de todo el mundo
-$(function(){
+$(document).ready(function(){
+  console.log("ready");
+
   var map;
   map = new jvm.Map({
     container: $('#world-map'),
@@ -12,7 +13,26 @@ $(function(){
     onRegionClick: function(event, code){
       console.log(event);
       console.log(code);
-      console.log(map.getRegionName(code));
-    }
+    },
   });
+
+  // The Toggle Off Canvas
+
+  $(function() {
+      // Toggle Nav on Click
+      $('.toggle-nav').click(function() {
+          // Calling a function in case you want to expand upon this.
+          toggleNav();
+          console.log("click!");
+      });
+  });
+  function toggleNav() {
+      if ($('#wrapper').hasClass('show-nav')) {
+          // Do things on Nav Close
+          $('#wrapper').removeClass('show-nav');
+      } else {
+          // Do things on Nav Open
+          $('#wrapper').addClass('show-nav');
+      }
+  }
 });
