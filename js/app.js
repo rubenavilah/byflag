e4b3f7fe8906592dc91fed18a560eba12e9849dc
rodrigotos <rodrigotos3@hotmail.com>
2016-10-22 12:42:36 -0500

    drill down from world map to colombian map

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-$(document).ready(function(){
 
+$(document).ready(function(){
+  // Variables from maps
   var map;
+  var coMap;
+  // Definition from world map
   map = new jvm.Map({
     container: $("#world-map"),
     map: "world_mill_en",
@@ -10,6 +13,7 @@ $(document).ready(function(){
       }
     },
     onRegionClick: function(event, code){
+      // Event click in some region
       console.log(event);
       console.log(code);
       var title = document.querySelector("#title");
@@ -22,6 +26,31 @@ $(document).ready(function(){
       $("#modal-info").modal("show");
     },
   });
+
+  // Definition from Colombian map
+  coMap = new jvm.Map({
+    container: $("#co-map"),
+    map: "co_mill",
+    regionStyle: {
+      initial: {
+        fill: "#B8E186"
+      }
+    },
+    onRegionClick: function(event, code){
+      // Event click in some region
+      console.log(event);
+      console.log(code);
+      var title = document.querySelector("#title");
+      var flag = document.querySelector("#flag");
+      var price = document.querySelector("#price");
+      //Modifed HTML elements
+      title.innerHTML = flags[code].name;
+      flag.src = flags[code].flag;
+      price.innerHTML = flags[code].price;
+      $("#modal-info").modal("show");
+    },
+  });
+
 
 var country = flags;
 for (var codeCountry in country) {
@@ -41,9 +70,7 @@ console.log(keys);
 var order = $("#order").on("click", function(){
     $("#country").empty();
 
-  });
-
-
+});
 
   // var a = [];
   // var b = [];
@@ -53,7 +80,35 @@ var order = $("#order").on("click", function(){
   //   var country = $("#country").append("<li>" + a + "</li>");
   // }
 
-});
+  // js Fucntion that execute the event click from view departments button
+  var viewDepartments = $("#view_departments").on("click", function(){
+      // Gets the divs from the maps
+      var coDiv = document.querySelector("#co-map");
+      var worldDiv = document.querySelector("#world-map");
+      //Display the colombian map
+      coDiv.removeAttribute('class', 'hidden');
+      // hide the world map
+      worldDiv.setAttribute('class', 'hidden');
+      // Hide the modal
+      $('#modal-info').modal('hide');
+  });
+
+  // js Fucntion that execute the event click from back button
+  var back = $("#back").on("click", function(){
+      // Gets the divs from the maps
+      var coDiv = document.querySelector("#co-map");
+      var worldDiv = document.querySelector("#world-map");
+      // hide the colombian map
+      coDiv.setAttribute('class', 'hidden');
+      //Display the world map
+      worldDiv.removeAttribute('class', 'hidden');
+  });
+
+
+
+});// End Document.Ready()
+
+
   // The Toggle Off Canvas
   $(function(){
       // Toggle Nav on Click
